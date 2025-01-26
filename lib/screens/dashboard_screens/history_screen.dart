7d6e6b0139329d0c7a187bffa4ad056d9db7ca6e
Shivam Sharma <86510204+AlphaShiv7781@users.noreply.github.com>
2025-01-27 00:36:02 +0530

    History Screen Added (#1)

---
+++ lib/screens/dashboard_screens/history_screen.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:instascan/custom_widgets/dashboard_option_card.dart';
+import 'package:instascan/custom_widgets/healthy_tipstrick_box.dart';
+
+class HistoryScreen extends StatefulWidget {
+  const HistoryScreen({super.key});
+
+  @override
+  State<HistoryScreen> createState() => _HistoryScreenState();
+}
+
+class _HistoryScreenState extends State<HistoryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white, // Set background color
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 40, 20, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Welcome Back!\nJohn Doe",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black, // Explicit text color
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                "Get your checkups",
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(height: 10),
+              InkWell(
+                onTap: (){
+
+                },
+                splashColor: Colors.cyan,
+
+                child: DashBoardOptionCard(
+                  title: 'Cancer Detection',
+                  subtitle: 'Skin cancer assessment using Skin lesion',
+                  icon: 'assets/images/skincancer.png',
+                  color: Colors.green,
+                ),
+              ),
+              const SizedBox(height: 10),
+              InkWell(
+                onTap: (){
+
+                },
+                splashColor: Colors.cyan,
+                child: DashBoardOptionCard(
+                  title: 'Pneumonia Detection',
+                  subtitle: 'Pneumonia assessment using Chest X-Ray',
+                  icon: 'assets/images/pneumonia.png',
+                  color: Colors.blue,
+                ),
+              ),
+              const SizedBox(height: 30),
+              const Text(
+                "Healthy Tips & Tricks",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(height: 10),
+              const HealthyTipsTrickBox(),
+            ],
+          ),
+        ),
+      ),
+    );;
+  }
+}
