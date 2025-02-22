7bc0ca2d5e4c22909ddce4d6a1b9d3687eba2de5
Shivam Sharma <86510204+AlphaShiv7781@users.noreply.github.com>
2025-02-22 12:50:32 +0530

    Feature/history screen (#8)
    * History Screen functionality added successfully
    
    * History Screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:instascan/firebase_options.dart';
+import 'package:instascan/screens/onboarding_screens/onboarding_screen.dart';
 import 'package:instascan/screens/splash_screen/splash_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 void main() async{
   await dotenv.load(fileName: '.env');
   WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  final onboarding = prefs.getBool('onboarding')??false;
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(MyApp(onboarding: onboarding,));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  const MyApp({super.key, required this.onboarding});
+  final bool onboarding ;
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -26,7 +30,8 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: SplashScreen(),
+        // home: SplashScreen(),
+        home: SplashScreen(onboarding: onboarding),
       ),
     );
   }
