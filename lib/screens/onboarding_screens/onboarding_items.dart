e193ee63e14887ff60935bec91961466a5ee3ba4
Shivam Sharma <86510204+AlphaShiv7781@users.noreply.github.com>
2025-02-22 13:13:56 +0530

    Development (#9)
    * History Screen functionality added successfully (#7)
    
    * Feature/history screen (#8)
    
    * History Screen functionality added successfully
    
    * History Screen
    
    * All done

---
+++ lib/screens/onboarding_screens/onboarding_items.dart
@@ -0,0 +1,39 @@
+
+import 'package:instascan/screens/onboarding_screens/onboarding_info.dart';
+
+class OnboardingItems{
+
+  List<OnboardingInfo> items = [
+    OnboardingInfo(
+        title: 'Medical Assistance',
+        description: 'Get Costlier Medical Assistance at your Doorstep',
+        imageUrl: 'assets/images/onboarding_assets/onboarding_2.jpg'),
+
+    OnboardingInfo(
+        title: 'Medical Assistance',
+        description: 'Get Costlier Medical Assistance at your Doorstep',
+        imageUrl: 'assets/images/onboarding_assets/onboarding_2.jpg'),
+
+    OnboardingInfo(
+        title: 'Medical Assistance',
+        description: 'Get Costlier Medical Assistance at your Doorstep',
+        imageUrl: 'assets/images/onboarding_assets/onboarding_2.jpg'),
+
+    OnboardingInfo(
+        title: 'Medical Assistance',
+        description: 'Get Costlier Medical Assistance at your Doorstep',
+        imageUrl: 'assets/images/onboarding_assets/onboarding_2.jpg'),
+
+    OnboardingInfo(
+        title: 'Medical Assistance',
+        description: 'Get Costlier Medical Assistance at your Doorstep',
+        imageUrl: 'assets/images/onboarding_assets/onboarding_2.jpg'),
+
+    OnboardingInfo(
+        title: 'Medical Assistance',
+        description: 'Get Costlier Medical Assistance at your Doorstep',
+        imageUrl: 'assets/images/onboarding_assets/onboarding_2.jpg'),
+
+  ];
+
+}
